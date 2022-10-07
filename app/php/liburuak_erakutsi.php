ef035755fb544a8e0e1d454fc173f497bd4b5574
Maits27 <maitiurlo@gmail.com>
2022-10-07 22:13:45 +0200

    zerrendatu liburuak (TODO)

--- app/php/liburuak_erakutsi.php
+++ app/php/liburuak_erakutsi.php
@@ -14,39 +14,7 @@
         die("Database connection failed: " . $conn->connect_error);
     }
     
-    
-  function cargardb($conn){
-  	$hostname = "db";
-  	$username = "admin";
-  	$password = "test";
-  	$db = "database.liburua";
-
-  	$conn = mysqli_connect($hostname,$username,$password,$db);
-  	if ($conn->connect_error) {
-   	 die("Database connection failed: " . $conn->connect_error);
-  	}
-  }
-  
-  function insert($conn){
-	$IZENBURUA = $_POST['IZENBURUA'];
-	$IDAZLEA = $_POST['IDAZLEA'];
-	$ARGITALPENDATA = $_POST['ARGITALPENDATA'];
-	$ORRIALDEKOP = $_POST['ORRIALDEKOP'];
-	$ARGITALETXEA = $_POST['ARGITALETXEA'];
-	$ISBN = $_POST['ISBN'];
-	$query = "INSERT INTO usuarios(IZENBURUA, IDAZLEA, ARGITALPENDATA, ORRIALDEKOP, ARGITALETXEA, ISBN) VALUES ('$IZENBURUA', '$IDAZLEA', '$ARGITALPENDATA', '$ORRIALDEKOP', '$ARGITALETXEA', '$ISBN')";
-	mysqli_query($conn, $query);
-}
-
-function delete($conn){
-	$ISBN = $_POST['ISBN'];
-	$query = "DELETE FROM liburua WHERE ISBN='$ISBN'";
-	mysqli_query($conn, $query);
-	//mysqli_close($conn);
-	
-}
-function cargarTabla($conn){
-	cargardb($conn);
+   
 	$query = mysqli_query($conn, "SELECT IZENBURUA, IDAZLEA FROM liburua")
    or die (mysqli_error($conn));
 
@@ -57,6 +25,6 @@ function cargarTabla($conn){
     	<td>{$row['IDAZLEA']}</td>
    	</tr><br>";
    	}
-   }
+ 
 
 ?>
