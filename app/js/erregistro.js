087deea7619773b0b4aaec2a52f0d455dab5b860
Aingeru Bellido <1405bellido@gmail.com>
2022-10-11 10:55:50 +0200

    Erregistro txarto

--- app/js/erregistro.js
+++ app/js/erregistro.js
@@ -1,15 +1,17 @@
-
 //https://desarrolloweb.com/articulos/1767.php
+
+const expresiones = {
+	izena: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	email: /[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/,
+	telefonoa: /[0-9]{1,9}$/
+}
+
+const texto_error="Erroreren bat dago sartutako datuetan. Hurrengo formatua erabili behar da: · Izen Abizenak: Testua soilik · NAN: 12345678-X · Telefonoa: 9 digitu";
 
 document.addEventListener("DOMContentLoaded", function(){
 	document.getElementById("formulario").addEventListener('submit', formularioaBalioztatu);
 });
 
-const expresiones = {
-	izena: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-	email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-	telefonoa: /^[0-9]{1,9}$/
-}
 
 function formularioaBalioztatu(evento){
 	evento.preventDefault();
@@ -19,17 +21,17 @@ function formularioaBalioztatu(evento){
 		return;
 	}
 	var iz = document.getElementById('Izena').value;
-	if(!expresiones.izena.test(iz.value)){
+	if(!expresiones.izena.test(iz)){
 		document.getElementById("erroreak").innerHTML="Izena txarto";
 		return;
 	}
 	var tel = document.getElementById('Telefonoa').value;
-	if(!expresiones.telefonoa.test(tel.value)){
+	if(!expresiones.telefonoa.test(tel)){
 		document.getElementById("erroreak").innerHTML="Telefonoa txarto";
 		return;
 	}
 	var ema = document.getElementById('email').value;
-	if(!expresiones.email.test(ema.value)){
+	if(!expresiones.email.test(ema)){
 		document.getElementById("erroreak").innerHTML="email txarto";
 		return;
 	}
