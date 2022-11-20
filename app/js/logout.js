5ba96b0dd371929735605bbe3a87a0c4ce6c5d49
Aingeru Bellido <1405bellido@gmail.com>
2022-11-20 11:07:53 +0100

    logout hobetu

--- app/js/logout.js
+++ app/js/logout.js
@@ -4,9 +4,8 @@ var id = window.setInterval(() => {
     document.onmousemove= function(){
         n=5
     };
-    console.log("pasa");
     n--;
     if(n <= -1){
-        location.href = "../config_php/logout.php";
+        location.href = "../../config_php/logout.php";
     }
 }, 1200);
